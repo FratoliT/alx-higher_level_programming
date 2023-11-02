1c898745c53034b6dd4567fadcfc11b53de7a8ef
fratolit <fratoli.tesfa@outlook.com>
2023-11-02 19:30:01 +0000

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,6 +2,5 @@
 """Locked class"""
 
 class LockedClass:
-"""prevents the user from intializing anything except first_name. """
-
+""" prevents the user from intializing anything except first_name. """
   __slots__ = ["first_name"]
